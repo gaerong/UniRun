bf62aca852dd7349df42c9e7e4b94b9895b40def
gaerong <pitchofpeach@gmail.com>
2022-10-07 16:28:38 +0900

    20221007

--- Uni-Run/Assets/02. Scripts/PlayController.cs
+++ Uni-Run/Assets/02. Scripts/PlayController.cs
@@ -60,6 +60,7 @@ public class PlayController : MonoBehaviour
     {
         // 애니메이터의 Die 트리거 파라미터를 셋
         animator.SetTrigger("Die");
+        
         // 오디오 소스에 할당된 오디오 클립을 deathClip으로 변경
         playerAudio.clip = deathClip;
         // 사망 효과음 재생
@@ -69,6 +70,9 @@ public class PlayController : MonoBehaviour
         playerRigidbody.velocity = Vector2.zero;
         // 사망 상태를 true로 변경
         isDead = true;
+
+        // 게임 매니저의 게임오버 처리 실행
+        GameManager.instance.OnPlayerDead();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
